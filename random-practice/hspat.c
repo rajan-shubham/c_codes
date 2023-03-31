d161b35b42f8fe419db1354adda6f0446b6f8b5f
rajan-shubham <SHUBHAMRAJAN@proton.me>
2023-03-31 10:55:31 +0530

    hollow square star pattern

---
+++ random-practice/hspat.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main(){
+    int n;
+    printf("enter the no. of rows: ");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if(i==1 ||i==n||j==1||j==n){
+                printf("*");
+            }else printf(" ");
+        }printf("\n");
+    }
+}
