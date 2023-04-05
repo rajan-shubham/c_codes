3ade6f682575c55a26172e3ab97ec06a92d382bd
rajan-shubham <SHUBHAMRAJAN@proton.me>
2023-04-05 13:22:02 +0530

    sum of even digit of given no.

---
+++ random-practice/soed.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main(){
+    int n,sum = 0,last;
+    printf("Enter any no. to find its sum of even digits : ");
+    scanf("%d",&n);
+    int num = n;
+    while(num>0){
+        
+        last=num%10;
+        if(last % 2 == 0){
+            sum = sum +last;
+        }
+        num=num/10;  
+    }
+    printf("The sum of even digit of %d is %d",n,sum);
+}
