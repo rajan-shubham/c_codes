ff9f60ee7696fe5db0915751d0193871fa11685b
rajan-shubham <SHUBHAMRAJAN@proton.me>
2023-05-29 11:55:37 +0530

    print string to integer & vice versa

--- string/strtoint.c
+++ string/strtoint.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 
 int main() {
-    int num = 12345;
-    char str[20];  // Assuming the string can hold up to 20 characters
+    int num1 = 12345;
+    char str1[20];  // Assuming the string can hold up to 20 characters
 
-    sprintf(str, "%d", num);
+    sprintf(str1, "%d", num1);
 
-    printf("Integer: %d\n", num);
-    printf("String: %s\n", str);
+    printf("Integer: %d\n", num1);
+    printf("String: %s\n", str1);
+
+    char str2[] = "6789";
+    int num2;
+
+    num2 = atoi(str2);
+
+    printf("String: %s\n", str2);
+    printf("Integer: %d\n", num2);
+
 
     return 0;
 }
